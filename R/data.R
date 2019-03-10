4360a5894e307e5cbc3cbba456c99c7799b6784a
Kanishka Misra <menogetusername@gmail.com>
2019-03-10 11:14:14 -0500

    pipes have been enabled

--- R/data.R
+++ R/data.R
@@ -1,10 +1,12 @@
 #' @title wmt18example
 #' @description Example references
+#' @format A data frame with 24288 rows and 5 variables:
 #' \describe{
-#'   \item{\code{system}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{id}}{integer COLUMN_DESCRIPTION}
-#'   \item{\code{candidate}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{reference}}{character COLUMN_DESCRIPTION}
+#' \item{\code{system}}{character COLUMN_DESCRIPTION}
+#' \item{\code{candidate_id}}{integer COLUMN_DESCRIPTION}
+#' \item{\code{reference_id}}{integer COLUMN_DESCRIPTION}
+#' \item{\code{candidate}}{character COLUMN_DESCRIPTION}
+#' \item{\code{reference}}{character COLUMN_DESCRIPTION}
 #'}
 #' @details DETAILS
 "wmt18example"
--- data-raw/get_data.R
+++ data-raw/get_data.R
@@ -1,9 +1,10 @@
 library(tidyverse)
 
+## TEMPORARY FOR NOW
 references_tibble <- tibble(
   reference = read_lines("data-raw/newstest2018-deen-ref-ts.en")
 ) %>%
-  mutate(id = row_number())
+  mutate(candidate_id = row_number(), reference_id = row_number())
 
 cands <- list.files("data-raw/", pattern = "*.de-en", full.names = T)
 
@@ -11,12 +12,11 @@ wmt18example <- map_dfr(cands, function(x) {
   return(tibble(candidate = read_lines(x) %>% set_names(.)))
 }, .id = "system") %>%
   group_by(system) %>%
-  mutate(id = row_number()) %>%
+  mutate(candidate_id = row_number()) %>%
   inner_join(references_tibble) %>%
   ungroup() %>%
   filter(reference != "NO REFERENCE AVAILABLE") %>%
-  select(system, id, candidate, reference)
+  select(system, candidate_id, reference_id, candidate, reference)
 
 usethis::use_data(wmt18example, overwrite = TRUE)
 sinew::makeOxygen(wmt18example)
-
